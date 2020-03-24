65638cdbf4683e59f0202e8e7cd3d5649f1badfe
takoda1 <31773273+takoda1@users.noreply.github.com>
2020-03-23 23:45:17 -0400

    Started restructuring of resources and links as served by nodejs

--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 let express = require('express')
 let app = express();
+let http = require('http');
 let path = require('path');
+let socketIO = require('socket.io');
+let server = http.Server(app); 
+
 
 require('dotenv').config();
 
+app.use(express.static(path.join(__dirname, "/public")));
+
 app.get('/', (req, res) => {
-    res.sendFile('home.html', {
-        root: path.join(__dirname, './')
-    });
+    res.sendFile(path.join(__dirname, 'home_page/home.html'));
 })
 
-var server = app.listen(process.env.PORT, () => {
-    console.log("App is running on port" + process.env.PORT)
+app.get('/chooseGame', (req, res) => {
+    res.sendFile(path.join(__dirname, 'home_page/choose_game.html'))
+})
+
+
+server.listen(process.env.PORT, () => {
+    console.log("Starting server on port " + process.env.PORT)
 })
