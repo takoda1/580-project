337b2585b57d32a41f1c4042f9a8497cc56f990c
carly20 <carly20@live.unc.edu>
2020-03-31 17:56:38 -0400

    Updated game choice page

---
+++ public/js/choose_game.js
@@ -0,0 +1,55 @@
+let games = ['Clap It', 'The Forbidden Forest', "Takoda's Game"]
+var index = 0;
+var selected = false;
+var first = true
+
+$(window).keyup(function(e){
+    if (first == true){
+        var msg = new SpeechSynthesisUtterance("Use the left and right arrow keys to navigate between games");
+        window.speechSynthesis.speak(msg);
+        first = false;
+    } else {
+        window.speechSynthesis.cancel();
+        if (selected == true && e.keyCode== 32){
+            if (index%3==0){
+                // jump to clap it
+            }else if (index%3==1) {
+                //jump to Forbidden Forest
+                location.href = '/forbiddenForest';
+            }else {
+                //jump to takoda's game
+            }
+        } else {
+            if (e.keyCode == 39) { 
+                index = index+1;
+            } else if (e.keyCode == 37) {
+                if (index > 0) {
+                    index = index-1;
+                } else {
+                    index = games.length-1;
+                }
+            }
+        }
+    }
+    var msg = new SpeechSynthesisUtterance("You are now selecting " + games[index%3]);
+    window.speechSynthesis.speak(msg);
+    msg = new SpeechSynthesisUtterance("Press the spacebar to play " + games[index%3]);
+    window.speechSynthesis.speak(msg);
+    selected = true;
+    addBorder();
+})
+
+function addBorder() {
+    var id = "";
+    if (index%3==0){
+        id = "clap_it";
+    }else if (index%3==1) {
+        id = "forbidden_forest";
+    }else {
+        id = "takoda_game";
+    }
+    document.getElementById("clap_it").style.border = "0px solid yellow";
+    document.getElementById("forbidden_forest").style.border = "0px solid yellow";
+    document.getElementById("takoda_game").style.border = "0px solid yellow";
+    document.getElementById(id).style.border = "15px solid yellow";
+}
